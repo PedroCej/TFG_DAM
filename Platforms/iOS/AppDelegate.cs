aeea94c3e86132a98bd29b477564e9bcb7b679b9
Pedro <pedrocejudo.v@gmail.com>
2024-05-14 09:53:50 +0200

    Agregar archivos de proyecto.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProyectoTFG
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
